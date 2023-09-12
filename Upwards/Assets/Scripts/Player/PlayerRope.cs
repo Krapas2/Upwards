2af85f617d4411058639f8e88c99def8a5ed646b
Krapas <eduardok04@gmail.com>
2023-09-12 00:45:12 -0300

    rope structure changes
    changing names of scripts for better understanding

---
+++ Upwards/Assets/Scripts/Player/PlayerRope.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRope : MonoBehaviour
+{
+
+    public RopeProjectile rope;
+    public float throwSpeed;
+    public float distanceOffsetFactor;
+
+    private Camera cam;
+
+    void Start(){
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire2")){
+            Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            float yOffset = Mathf.Pow(Vector2.Distance(transform.position, mousePos),2) / distanceOffsetFactor;
+
+            RopeProjectile curRope = Instantiate(rope, transform.position, Quaternion.identity);
+            curRope.rb.velocity = ((mousePos - (Vector2)transform.position) + new Vector2(0, yOffset)).normalized * throwSpeed;
+        }
+    }
+}
