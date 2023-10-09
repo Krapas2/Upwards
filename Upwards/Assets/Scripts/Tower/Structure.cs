6e3a14c6bf6aae46e5fd8ac8ae63bd5d79de2fda
Eduardo Krapas Antonio <eduardok04@gmail.com>
2023-10-09 19:08:06 -0300

    added bombs

--- Upwards/Assets/Scripts/Tower/Structure.cs
+++ Upwards/Assets/Scripts/Tower/Structure.cs
@@ -7,10 +7,10 @@ public class Structure : MonoBehaviour
 
     public bool built = false;
     public LayerMask collisions; // objects that don't allow the structure to be built on same position as them
-    public Color canBuildColor; 
+    public Color canBuildColor;
     public Color cannotBuildColor;
 
-    public Transform groundCheck; 
+    public Transform groundCheck;
     public LayerMask ground;
 
     public Transform[] snapChecks; // position to check for the structure having support (ground or other)
@@ -34,35 +34,43 @@ public class Structure : MonoBehaviour
 
     void Update()
     {
-        if(!built){
-            if (CanBeBuilt() && playerBuild.canBeAfforded()){
+        if (!built)
+        {
+            if (CanBeBuilt() && playerBuild.canBeAfforded())
+            {
                 // visual for structure can be built
-                spriteRen.color = canBuildColor; 
-                foreach(SpriteRenderer childRen in childrenRen){
+                spriteRen.color = canBuildColor;
+                foreach (SpriteRenderer childRen in childrenRen)
+                {
                     childRen.color = canBuildColor;
                 }
 
-                if (Input.GetButton("Fire1")){
+                if (Input.GetButton("Fire1"))
+                {
                     // visual for structure is built
                     spriteRen.sortingOrder = -3;
                     spriteRen.color = new Color(1, 1, 1);
-                    foreach(SpriteRenderer childRen in childrenRen){
+                    foreach (SpriteRenderer childRen in childrenRen)
+                    {
                         spriteRen.sortingOrder = -2;
                         childRen.color = new Color(1, 1, 1);
                     }
-                    
+
                     // background changes
                     gameObject.AddComponent<BoxCollider2D>().isTrigger = true;
                     playerBuild.applyCost();
                     playerBuild.currentStructure = null;
                     built = true;
 
-                    if(audioManager) // only play sound if audio manager exists. otherwise game crashes
+                    if (audioManager) // only play sound if audio manager exists. otherwise game crashes
                         audioManager.Play("Construir");
                 }
-            } else {
+            }
+            else
+            {
                 // visual can't be built
-                foreach(SpriteRenderer childRen in childrenRen){
+                foreach (SpriteRenderer childRen in childrenRen)
+                {
                     childRen.color = cannotBuildColor;
                 }
                 spriteRen.color = cannotBuildColor;
@@ -71,19 +79,23 @@ public class Structure : MonoBehaviour
     }
 
     //structures can be built if the space they occupy isn't already used and they are either on the ground or built on other structures
-    private bool CanBeBuilt(){ 
-        
+    private bool CanBeBuilt()
+    {
+
         Collider2D[] cols = Physics2D.OverlapBoxAll(transform.position, spriteRen.sprite.bounds.extents * 2 - new Vector3(.125f, .125f, 0), 0, collisions);
         bool blocked = false;
-        foreach(Collider2D col in cols){
+        foreach (Collider2D col in cols)
+        {
             blocked |= col.gameObject != gameObject;
         }
 
         bool based = Physics2D.OverlapCircle(groundCheck.position, 0f, ground);
 
         bool snapped = false;
-        foreach(Structure structure in FindObjectsOfType<Structure>()){
-            foreach(Transform snapCheck in snapChecks){
+        foreach (Structure structure in FindObjectsOfType<Structure>())
+        {
+            foreach (Transform snapCheck in snapChecks)
+            {
                 snapped |= structure.transform.position == snapCheck.position;
             }
         }
--- Upwards/Assets/Scripts/Visual/VignetteSize.cs
+++ Upwards/Assets/Scripts/Visual/VignetteSize.cs
@@ -23,17 +23,17 @@ public class VignetteSize : MonoBehaviour
     {
         float vignetteSize;
         vignetteSize = Mathf.Clamp(Map(player.position.y, maxHeight, minHeight, minSize, maxSize), minSize, maxSize);
-        Debug.Log(vignetteSize);
 
         image.material.SetFloat("_WindowSize", vignetteSize);
         image.material.SetVector("_PlayerPos", player.position - transform.position);
     }
 
-    public float Map(float OldValue, float OldMin, float OldMax, float NewMin, float NewMax){
+    public float Map(float OldValue, float OldMin, float OldMax, float NewMin, float NewMax)
+    {
         float OldRange = OldMax - OldMin;
         float NewRange = NewMax - NewMin;
         float NewValue = ((OldValue - OldMin) * NewRange / OldRange) + NewMin;
-        
+
         return NewValue;
     }
 }
