f83569f9f34526772070663f5d93d572a0acfab1
PedroSagradim <sagradimpedro@gmail.com>
2023-11-12 01:49:55 -0300

    -added AudioMixer -added OptionsScreen with volume management -modified Sound scripts (added AudioMixerGroup on sounds and SFX)

---
+++ Upwards/Assets/Scripts/Audio/SliderVolume.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SliderVolume : MonoBehaviour
+{
+
+    [SerializeField] private Slider slider;
+    public AudioMixer audioMixer;
+    
+    public void SetMasterLvl(float masterLvl){
+        audioMixer.SetFloat("MasterLevel",masterLvl);
+    }
+    public void SetSfxLvl(float sfxLvl){
+        audioMixer.SetFloat("SFXLevel",sfxLvl);
+    }
+    public void SetMusicLvl(float musicLvl){
+        audioMixer.SetFloat("SongLevel",musicLvl);
+    }
+}
